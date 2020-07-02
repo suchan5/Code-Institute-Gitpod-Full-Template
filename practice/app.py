ead46ab3d420e0b48046665f53da26785d0582bd
suchan5 <suchan5@hotmail.com>
2020-07-02 16:48:47 +0000

    practice

--- practice/app.py
+++ practice/app.py
@@ -6,47 +6,74 @@ import random
 app = Flask(__name__)
 
 
-# create employee
-@app.route('/create')
-def create_employee():
+@app.route('/')
+def home():
+    return 'Welcome!'
+
+
+@app.route('/create/members')
+def create_members():
     return render_template('create.template.html')
 
 
-@app.route('/create', methods=['POST'])
-def process_create_employee():
+@app.route('/create/members', methods=['POST'])
+def process_members():
     print(request.form)
     with open('data.csv', 'a', newline="\n") as fp:
         writer = csv.writer(fp, delimiter=",")
         id = random.randint(10000, 99999)
-        member_name = request.form.get('member-name')
+        name = request.form.get('member-name')
         nickname = request.form.get('nickname')
         position = request.form.get('position')
-        writer.writerow([id, member_name, nickname, position])
-    return redirect(url_for('read_employee'))
+        writer.writerow([id, name, nickname, position])
+    return redirect(url_for('read_members'))
 
 
-# read  employees
-@app.route('/employees')
-def read_employee():
-    all_emplyoees = read_employees_from_file()
-    return render_template('read_employee.template.html',
-                           employees=all_emplyoees
+@app.route('/members')
+def read_members():
+    all_members = read_members_from_file()
+    return render_template('read.template.html',
+                           members=all_members
                            )
 
 
-def read_employees_from_file():
-    all_employees = []
+@app.route('/update/<member_id>')
+def update_member(member_id):
+    editing_member = find_member_by_id(member_id)
+    return render_template('update.template.html',
+                           member=editing_member)
+
+
+def read_members_from_file():
+    all_members = []
     with open('data.csv', 'r', newline="\n") as fp:
         reader = csv.reader(fp, delimiter=",")
         next(reader)
         for line in reader:
-            all_employees.append({
+            all_members.append({
                 'id': line[0],
                 'member_name': line[1],
                 'nickname': line[2],
                 'position': line[3]
             })
-    return all_employees
+    return all_members
+
+
+def find_member_by_id(member_id):
+    editing_member = None
+    with open('data.csv', 'r', newline="\n") as fp:
+        reader = csv.reader(fp, delimiter=",")
+        next(reader)
+        for line in reader:
+            if member_id == line[0]:
+                editing_member = {
+                    'id': line[0],
+                    'member_name': line[1],
+                    'nickname': line[2],
+                    'position': line[3]
+                }
+            break
+    return editing_member
 
 
 # "magic code" -- boilerplate
